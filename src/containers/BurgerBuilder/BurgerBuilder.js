a2d082edde0b8b4d16534a6e0d2b16439fb8325f
krittiyaclark <krittiyaclark@gmail.com>
2020-05-14 20:10:46 -0400

    Fetching Orders and Outputting the Orders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,25 +91,39 @@ class BurgerBuilder extends Component {
 
 	purchaseContinueHander = () => {
 		// alert('You are Continue');
-		this.setState({ loading: true });
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Krittiya Clark',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '41351',
-					country: 'USA',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
-		};
-		axios
-			.post('/orders.json', order)
-			.then((response) => this.setState({ loading: false, purchasing: false }))
-			.catch((error) => this.setState({ loading: false, purchasing: false }));
+		// this.setState({ loading: true });
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Krittiya Clark',
+		// 		address: {
+		// 			street: 'Teststreet 1',
+		// 			zipCode: '41351',
+		// 			country: 'USA',
+		// 		},
+		// 		email: 'test@test.com',
+		// 	},
+		// 	deliveryMethod: 'fastest',
+		// };
+		// axios
+		// 	.post('/orders.json', order)
+		// 	.then((response) => this.setState({ loading: false, purchasing: false }))
+		// 	.catch((error) => this.setState({ loading: false, purchasing: false }));
+		const queryParams = [];
+		for (let i in this.state.ingredients) {
+			queryParams.push(
+				encodeURIComponent(i) +
+					'=' +
+					encodeURIComponent(this.state.ingredients[i])
+			);
+		}
+		queryParams.push('price=' + this.state.totalPrice);
+		const queryString = queryParams.join('&');
+		this.props.history.push({
+			pathname: '/checkout',
+			search: '?' + queryString,
+		});
 	};
 
 	render() {
