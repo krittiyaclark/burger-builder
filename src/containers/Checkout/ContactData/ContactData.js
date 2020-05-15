5420b91dbf8f221df391b2d983924e0f266488d1
krittiyaclark <krittiyaclark@gmail.com>
2020-05-14 23:46:12 -0400

    Added input form,  handling user input handling form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,16 +2,63 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: '',
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name',
+				},
+				value: '',
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street',
+				},
+				value: '',
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code',
+				},
+				value: '',
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country',
+				},
+				value: '',
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your E-Mail',
+				},
+				value: '',
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'fastest', displayValue: 'Fastest' },
+						{ value: 'cheapest', displayValue: 'Cheapest' },
+					],
+				},
+				value: '',
+			},
 		},
 		loading: false,
 	};
@@ -19,20 +66,17 @@ class ContactData extends Component {
 	orderHandler = (event) => {
 		event.preventDefault();
 		console.log(this.props.ingredients);
+		const formData = {};
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[
+				formElementIdentifier
+			].value;
+		}
 		this.setState({ loading: true });
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
-			customer: {
-				name: 'Calvin Clark',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '41351',
-					country: 'USA',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
+			orderForm: formData,
 		};
 		axios
 			.post('/orders.json', order)
@@ -45,33 +89,39 @@ class ContactData extends Component {
 			});
 	};
 
+	inputChangedHandler = (event, inputIdentifier) => {
+		const updatedOrderForm = {
+			...this.state.orderForm,
+		};
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier],
+		};
+		updatedFormElement.value = event.target.value;
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		this.setState({ orderForm: updatedOrderForm });
+	};
+
 	render() {
+		// turn orderForm object here to array
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key],
+			});
+			console.log(formElementsArray);
+		}
 		let form = (
-			<form>
-				<input
-					className={classes.Input}
-					type='text'
-					name='name'
-					placeholder='Your Name'
-				/>
-				<input
-					className={classes.Input}
-					type='email'
-					name='email'
-					placeholder='Your Mail'
-				/>
-				<input
-					className={classes.Input}
-					type='text'
-					name='street'
-					placeholder='Street'
-				/>
-				<input
-					className={classes.Input}
-					type='text'
-					name='postal'
-					placeholder='Postal Code'
-				/>
+			<form onSubmit={this.orderHandler}>
+				{formElementsArray.map((formElement) => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						changed={(event) => this.inputChangedHandler(event, formElement.id)}
+					/>
+				))}
 				<Button btnType='Success' clicked={this.orderHandler}>
 					ORDER
 				</Button>
